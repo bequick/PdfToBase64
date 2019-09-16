51b30badb6909b87708bb2386700f1024158fd4a
bequick <mdonoso2007@gmail.com>
2019-09-15 22:41:43 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace PdbBase64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtPathFile = new System.Windows.Forms.TextBox();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnToBase64 = new System.Windows.Forms.Button();
+            this.btnToTxt = new System.Windows.Forms.Button();
+            this.btnToPdf = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnToPdf);
+            this.panel1.Controls.Add(this.btnToTxt);
+            this.panel1.Controls.Add(this.btnToBase64);
+            this.panel1.Controls.Add(this.btnLoadFile);
+            this.panel1.Controls.Add(this.txtPathFile);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1026, 53);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtPathFile
+            // 
+            this.txtPathFile.Location = new System.Drawing.Point(13, 13);
+            this.txtPathFile.Name = "txtPathFile";
+            this.txtPathFile.Size = new System.Drawing.Size(448, 20);
+            this.txtPathFile.TabIndex = 0;
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(480, 13);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFile.TabIndex = 1;
+            this.btnLoadFile.Text = "...";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.BtnLoadFile_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtEditor);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 53);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1026, 397);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtEditor.Location = new System.Drawing.Point(0, 0);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.Size = new System.Drawing.Size(1026, 397);
+            this.txtEditor.TabIndex = 0;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Title = "Abrir archivo pdf";
+            // 
+            // btnToBase64
+            // 
+            this.btnToBase64.Location = new System.Drawing.Point(574, 13);
+            this.btnToBase64.Name = "btnToBase64";
+            this.btnToBase64.Size = new System.Drawing.Size(75, 23);
+            this.btnToBase64.TabIndex = 2;
+            this.btnToBase64.Text = "to Base 64";
+            this.btnToBase64.UseVisualStyleBackColor = true;
+            this.btnToBase64.Click += new System.EventHandler(this.BtnToBase64_Click);
+            // 
+            // btnToTxt
+            // 
+            this.btnToTxt.Location = new System.Drawing.Point(655, 12);
+            this.btnToTxt.Name = "btnToTxt";
+            this.btnToTxt.Size = new System.Drawing.Size(75, 23);
+            this.btnToTxt.TabIndex = 3;
+            this.btnToTxt.Text = "to txt";
+            this.btnToTxt.UseVisualStyleBackColor = true;
+            this.btnToTxt.Click += new System.EventHandler(this.BtnToTxt_Click);
+            // 
+            // btnToPdf
+            // 
+            this.btnToPdf.Location = new System.Drawing.Point(737, 13);
+            this.btnToPdf.Name = "btnToPdf";
+            this.btnToPdf.Size = new System.Drawing.Size(75, 23);
+            this.btnToPdf.TabIndex = 4;
+            this.btnToPdf.Text = "to pdf";
+            this.btnToPdf.UseVisualStyleBackColor = true;
+            this.btnToPdf.Click += new System.EventHandler(this.BtnToPdf_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.TextBox txtPathFile;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtEditor;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnToBase64;
+        private System.Windows.Forms.Button btnToTxt;
+        private System.Windows.Forms.Button btnToPdf;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace PdbBase64
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"c:\",
+                Title = "Agregar archivo PDF",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "pdf",
+                Filter = "pdf files (*.pdf) | *.pdf",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtPathFile.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void BtnToBase64_Click(object sender, EventArgs e)
+        {
+            byte[] pdfBytes = File.ReadAllBytes(txtPathFile.Text);
+            string pdfBase64 = Convert.ToBase64String(pdfBytes);
+            txtEditor.Text = pdfBase64;
+        }
+
+        private void BtnToTxt_Click(object sender, EventArgs e)
+        {
+            string path = Path.GetFullPath(txtPathFile.Text);
+            string fileName = Path.GetFileName(txtPathFile.Text);
+            string fileNameTxt = path + fileName.Replace("pdf", "txt");
+            string saveText = txtEditor.Text;
+            File.WriteAllText(fileNameTxt, saveText);
+        }
+
+        private void BtnToPdf_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = Convert.FromBase64String(txtEditor.Text);
+            string pathPdf = @"c:\Temp\resultado.pdf";
+
+            FileStream stream = new FileStream(pathPdf, FileMode.CreateNew);
+            BinaryWriter writer = new BinaryWriter(stream);
+            writer.Write(bytes,0,bytes.Length);
+            writer.Close();
+        }
+    }
+}
